d4e27a0fb56c3274cd8ba7815a22e525456a7225
lucaradio24 <lukaabeck3@gmail.com>
2025-09-15 12:40:59 +0200

    Add sorting functionality to task list and update UI

--- src/index.js
+++ src/index.js
@@ -4,21 +4,20 @@ import { Task } from "./tasks.js";
 import { Category } from "./categories.js";
 import { Store } from "./store.js";
 
-
-
 // Wiring
 const newTaskForm = document.querySelector("#new-task-form");
 const newTaskDialog = document.querySelector("#new-task-dialog");
 const newTaskBtn = document.querySelector("#new-task-btn");
 const taskList = document.querySelector(".task-list");
-const cancelBtn = document.querySelector('#cancel-btn');
+const cancelBtn = document.querySelector("#cancel-btn");
 const categoriesList = document.querySelector(".categories-list");
 const newCategoryBtn = document.querySelector("#new-category-btn");
 const newCategoryForm = document.querySelector("#new-category-form");
 const categorySelected = document.querySelector(".category-selected");
-const allBtn = document.querySelector('#show-all');
-const completedBtn = document.querySelector('#show-completed');
-const uncompletedBtn = document.querySelector('#show-uncompleted');
+const allBtn = document.querySelector("#show-all");
+const completedBtn = document.querySelector("#show-completed");
+const uncompletedBtn = document.querySelector("#show-uncompleted");
+const sortDropdown = document.querySelector("#sort-dropdown");
 
 // Init app
 const store = new Store([]);
@@ -26,16 +25,16 @@ store.createCategory("Personale");
 store.selectCategory("Personale");
 renderCategories();
 
+// Variabili globali
 let editingTaskId = null;
-let activeFilter = 'all';
-
+let activeFilter = "all";
+let activeSort;
+const priorityOrder = { Bassa: 1, Media: 2, Alta: 3 };
 
 // Bottone per la modale + form submit listener del Nuovo task
 newTaskBtn.addEventListener("click", () => {
   newTaskDialog.showModal();
 });
-
-
 
 newTaskForm.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -43,34 +42,34 @@ newTaskForm.addEventListener("submit", (e) => {
   const title = document.querySelector("#task-title").value;
   const desc = document.querySelector("#task-desc").value;
   const dueDate = document.querySelector("#task-date").value;
-  const priority = document.querySelector("input[name='priority']:checked")?.value || 'Media';
-  
-  if(editingTaskId) {
-   store.getSelectedCategory().updateTask(editingTaskId, {
-    title: title,
-    desc: desc,
-    dueDate: dueDate,
-    priority: priority,
-   });
-   renderTasks(store.getSelectedCategory().name);
-   newTaskDialog.close()
-   editingTaskId = null;
-  } else {
+  const priority =
+    document.querySelector("input[name='priority']:checked")?.value || "Media";
 
-  store.getSelectedCategory().createTask({
-    title: title,
-    desc: desc,
-    dueDate: dueDate,
-    priority: priority,
-    isCompleted: false,
-  });
-  renderTasks(store.getSelectedCategory().name);
-  newTaskDialog.close();
-  newTaskForm.reset();
+  if (editingTaskId) {
+    store.getSelectedCategory().updateTask(editingTaskId, {
+      title: title,
+      desc: desc,
+      dueDate: dueDate,
+      priority: priority,
+    });
+    renderTasks(store.getSelectedCategory().name);
+    newTaskDialog.close();
+    editingTaskId = null;
+  } else {
+    store.getSelectedCategory().createTask({
+      title: title,
+      desc: desc,
+      dueDate: dueDate,
+      priority: priority,
+      isCompleted: false,
+    });
+    renderTasks(store.getSelectedCategory().name);
+    newTaskDialog.close();
+    newTaskForm.reset();
   }
 });
 
-cancelBtn.addEventListener('click', () => newTaskDialog.close())
+cancelBtn.addEventListener("click", () => newTaskDialog.close());
 
 // Funzione per mostrare i tasks
 
@@ -78,74 +77,108 @@ function renderTasks(name) {
   taskList.innerHTML = "";
 
   const category = store.getCategories().find((cat) => cat.name === name);
-  let tasksToShow = '';
-  if (activeFilter === 'all'){
+  let tasksToShow = "";
+  if (activeFilter === "all") {
     tasksToShow = category.getTasks();
-  } else if(activeFilter === 'completed'){
+  } else if (activeFilter === "completed") {
     tasksToShow = category.getCompletedTasks();
   } else {
     tasksToShow = category.getUncompletedTasks();
   }
 
-    tasksToShow.forEach((t) => {
-      let task = document.createElement("div");
-      task.classList.add("task");
-      task.innerHTML = `<h2>${t.title}</h2>
+  if (activeSort === "date-inc") {
+    tasksToShow = tasksToShow.toSorted(
+      (a, b) => new Date(a.dueDate) - new Date(b.dueDate)
+    );
+  } else if (activeSort === "date-dec") {
+    tasksToShow = tasksToShow.toSorted(
+      (a, b) => new Date(b.dueDate) - new Date(a.dueDate)
+    );
+  } else if (activeSort === "priority-inc") {
+    tasksToShow = tasksToShow.toSorted(
+      (a, b) => priorityOrder[a.priority] - priorityOrder[b.priority]
+    );
+  } else if (activeSort === "priority-dec") {
+    tasksToShow = tasksToShow.toSorted(
+      (a, b) => priorityOrder[b.priority] - priorityOrder[a.priority]
+    );
+  }
+
+  tasksToShow.forEach((t) => {
+    let task = document.createElement("div");
+    task.classList.add("task");
+    task.innerHTML = `<h2>${t.title}</h2>
       <p> ${t.desc} </p>
       <h3>${t.dueDate}</h3>
       <h4>${t.priority}</h4> 
       <label for='complete'>Completato</label>
       <input type="checkbox" id='complete' class="complete-checkbox" ${
-        t.isCompleted ? "checked" : ""}>
+        t.isCompleted ? "checked" : ""
+      }>
         <button class='edit-btn'>Modifica</button>
         <button class='del-btn'>Elimina</button>`;
-        taskList.appendChild(task);
-      
+    taskList.appendChild(task);
+
     if (t.isCompleted) {
       task.classList.add("completed");
     }
-    
+
     const checkbox = task.querySelector(".complete-checkbox");
     checkbox.addEventListener("change", () => {
       t.toggleCompleted();
       renderTasks(name);
     });
 
-    const deleteBtn = task.querySelector('.del-btn');
-    deleteBtn.addEventListener('click', () => {
-        category.deleteTask(t.id);
-        renderTasks(name)
-    })
+    const deleteBtn = task.querySelector(".del-btn");
+    deleteBtn.addEventListener("click", () => {
+      category.deleteTask(t.id);
+      renderTasks(name);
+    });
 
-    const editBtn = task.querySelector('.edit-btn');
-    editBtn.addEventListener('click', () => {
-    const formSubmitBtn = document.querySelector('#form-submit')
-        formSubmitBtn.textContent = 'Modifica'
-        editingTaskId = t.id;   
-        document.querySelector('#task-title').value = t.title;
-        document.querySelector('#task-desc').value = t.desc;
-        document.querySelector('#task-date').value = t.dueDate;
-        document.querySelector(`input[name='priority'][value='${t.priority}']`).checked = true;
-        newTaskDialog.showModal()
-    })
+    const editBtn = task.querySelector(".edit-btn");
+    editBtn.addEventListener("click", () => {
+      const formSubmitBtn = document.querySelector("#form-submit");
+      formSubmitBtn.textContent = "Modifica";
+      editingTaskId = t.id;
+      document.querySelector("#task-title").value = t.title;
+      document.querySelector("#task-desc").value = t.desc;
+      document.querySelector("#task-date").value = t.dueDate;
+      document.querySelector(
+        `input[name='priority'][value='${t.priority}']`
+      ).checked = true;
+      newTaskDialog.showModal();
+    });
   });
 }
 
 // Bottoni filtri
-allBtn.addEventListener('click', () => {
-  activeFilter = 'all';
-  renderTasks(store.getSelectedCategory().name)
-})
-completedBtn.addEventListener('click', () => {
-  activeFilter = 'completed'
-    renderTasks(store.getSelectedCategory().name)
-
-})
-uncompletedBtn.addEventListener('click', () => {
-  activeFilter = 'uncompleted'
-    renderTasks(store.getSelectedCategory().name)
-
-})
+allBtn.addEventListener("click", () => {
+  activeFilter = "all";
+  renderTasks(store.getSelectedCategory().name);
+});
+completedBtn.addEventListener("click", () => {
+  activeFilter = "completed";
+  renderTasks(store.getSelectedCategory().name);
+});
+uncompletedBtn.addEventListener("click", () => {
+  activeFilter = "uncompleted";
+  renderTasks(store.getSelectedCategory().name);
+});
+// Dropdown
+sortDropdown.addEventListener("change", () => {
+  if (sortDropdown.value === "date-inc") {
+    activeSort = "date-inc";
+  } else if (sortDropdown.value === "priority-inc") {
+    activeSort = "priority-inc";
+  } else if (sortDropdown.value === "date-dec") {
+    activeSort = "date-dec";
+  } else if (sortDropdown.value === "priority-dec") {
+    activeSort = "priority-dec";
+  } else {
+    activeSort = null;
+  }
+  renderTasks(store.getSelectedCategory().name);
+});
 
 newCategoryBtn.addEventListener("click", (e) => {
   e.preventDefault();
